549a99a0112506826cfbcb75c4786919da3d8699
rMi99 <99ramigimhama@gmail.com>
2023-10-07 00:31:53 +0530

    relove search

--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -35,37 +35,31 @@ const TaskTable = () => {
     id: null,
     task: '',
     description: '',
-  
   });
   const [addDialogOpen, setAddDialogOpen] = useState(false);
   const [newTodo, setNewTodo] = useState({
     task: '',
     description: '',
     user_id: userId,
-    
   });
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');  
   const [searchId, setSearchId] = useState('');
-  const [searchedTask, setSearchedTask] = useState(null);
-  // const token = localStorage.getItem('token');
-  
-
+  // const [searchedTask, setSearchedTask] = useState('');
+  const [filteredTasks, setFilteredTasks] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
     axios
-      .get(`http://localhost:8000/api/task/${userId}`
-    )
+      .get(`http://localhost:8000/api/task/${userId}`)
       .then((response) => {
         setTasks(response.data);
+        setFilteredTasks(response.data); // Initialize filteredTasks with all tasks
         setIsLoading(false);
-
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
         setIsLoading(false);
-
       });
   }, [userId]);
 
@@ -79,13 +73,11 @@ const TaskTable = () => {
       task: '',
       description: '',
       user_id: userId,
-      
     });
   };
 
   const handleAddTodo = () => {
     setIsLoading(true);
-  
 
     if (newTodo.task.trim() === '') {
       setIsLoading(false);
@@ -93,18 +85,23 @@ const TaskTable = () => {
       setSnackbarOpen(true);
       return;
     }
-      
   
     axios
       .post('http://localhost:8000/api/create', newTodo)
       .then(() => {
         axios
-        .get(`http://localhost:8000/api/task/${userId}`,
-          
-          )
+          .get(`http://localhost:8000/api/task/${userId}`)
           .then((response) => {
             setTasks(response.data);
+            setFilteredTasks(response.data); 
             setIsLoading(false);
+            Swal.fire({
+              icon: 'success',
+              title: 'Success!',
+              text: 'Task added successfully',
+              showConfirmButton: false,
+              timer: 2000, 
+            });
           })
           .catch((error) => {
             console.error('Error fetching updated data:', error);
@@ -117,10 +114,8 @@ const TaskTable = () => {
         setIsLoading(false);
       });
   };
-  
 
   const handleDelete = (id) => {
-
     setTaskToDeleteId(id);
     setDeleteDialogOpen(true);
   };
@@ -142,19 +137,16 @@ const TaskTable = () => {
         .delete(`http://localhost:8000/api/delete/${taskToDeleteId}`)
         .then(() => {
           setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskToDeleteId));
+          setFilteredTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskToDeleteId));
           setDeleteSuccess(true);
           setTaskToDeleteId(null);
-        setIsLoading(false);
-
+          setIsLoading(false);
         })
         .catch((error) => {
           console.error('Error deleting task:', error);
-        setIsLoading(false);
-
+          setIsLoading(false);
         });
     }
-    setIsLoading(false);
-
     setDeleteDialogOpen(false);
   };
 
@@ -179,18 +171,16 @@ const TaskTable = () => {
           .get(`http://localhost:8000/api/task/${userId}`)
           .then((response) => {
             setTasks(response.data);
-        setIsLoading(false);
-
+            setFilteredTasks(response.data); // Update filteredTasks after updating a task
+            setIsLoading(false);
           })
           .catch((error) => {
             console.error('Error fetching updated data:', error);
             setIsLoading(false);
-         
-        });
+          });
       })
       .catch((error) => {
         setIsLoading(false);
-
         console.error('Error updating task:', error);
       });
   };
@@ -205,212 +195,169 @@ const TaskTable = () => {
   const handleSearch = () => {
     setIsLoading(true);
 
-    axios
-      .get(`http://localhost:8000/api/show/${searchId}/${userId}`)
-      .then((response) => {
-        setSearchedTask(response.data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching task by ID:', error);
-        setSearchedTask(null);
-        setIsLoading(false);
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'No Task Found!!',
-        });
 
-      });
+    const filtered = tasks.filter((task) =>
+      task.task.toLowerCase().includes(searchId.toLowerCase())
+    );
+
+    setFilteredTasks(filtered);
+
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    handleSearch();
+  }, [searchId]);
 
   return (
     <div>
- {isLoading ? (
+      {isLoading ? (
         <Spinner />
       ) : (
-<div>
-      <Button variant="outlined" color="primary" sx={{ mt: 1 }} onClick={handleOpenAddDialog}>
-        Add Todo
-      </Button>
+        <div>
+          <Button variant="outlined" color="primary" sx={{ mt: 1 }} onClick={handleOpenAddDialog}>
+            Add Todo
+          </Button>
 
-    
-      <TextField
-        label="Search by ID"
-        variant="outlined"
-        fullWidth
-        value={searchId}
-        onChange={(e) => setSearchId(e.target.value)}
-        sx={{ mt: 1 }}
-      />
-      <Button variant="outlined" color="primary" onClick={handleSearch}>
-        Search
-      </Button>
-
-      <Grid container spacing={2} sx={{ mt: 2 }}>
-        {searchedTask ? (
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-             
-                <Typography variant="h5" component="div">
-                  {searchedTask.task}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {searchedTask.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <IconButton onClick={() => handleUpdate(searchedTask)} color="success">
-                  <EditIcon />
-                </IconButton>
-                <IconButton onClick={() => handleDelete(searchedTask.id)} color="error">
-                  <DeleteIcon />
-                </IconButton>
-              </CardActions>
-            </Card>
+          <TextField
+            label="Search by Task Name"
+            variant="outlined"
+            fullWidth
+            value={searchId}
+            onChange={(e) => setSearchId(e.target.value)}
+            sx={{ mt: 1 }}
+          />
+          <Grid container spacing={2} sx={{ mt: 2 }}>
+            {filteredTasks.map((task) => (
+              <Grid item xs={8} md={4} lg={2} key={task.id}>
+                <Card>
+                  <CardContent>
+                    <Avatar alt="avatar" src="https://mui.com/static/images/avatar/1.jpg" />
+                    <Typography variant="h5" component="div">
+                      {task.task}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {task.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <IconButton onClick={() => handleUpdate(task)} color="success">
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton onClick={() => handleDelete(task.id)} color="error">
+                      <DeleteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
-        ) : (
-          tasks.map((task) => (
 
-            <Grid item xs={8} md={4} lg={2} key={task.id}>
-              <Card>
-                <CardContent>
-                <Avatar alt="avatar" src="https://mui.com/static/images/avatar/1.jpg" />
-                  <Typography variant="h5" component="div">
-                    {task.task}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {task.description}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <IconButton onClick={() => handleUpdate(task)} color="success">
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton onClick={() => handleDelete(task.id)} color="error">
-                    <DeleteIcon />
-                  </IconButton>
-                </CardActions>
-              </Card>
-            </Grid>
+          <Snackbar open={deleteSuccess} autoHideDuration={3000} onClose={handleDeleteAlertClose}>
+            <Alert onClose={handleDeleteAlertClose} severity="success">
+              Task deleted successfully!
+            </Alert>
+          </Snackbar>
 
+          <Dialog
+            open={deleteDialogOpen}
+            onClose={handleCancelDelete}
+            aria-labelledby="delete-dialog-title"
+            aria-describedby="delete-dialog-description"
+          >
+            <DialogTitle id="delete-dialog-title">Confirm Delete</DialogTitle>
+            <DialogContent>Are you sure you want to delete this task?</DialogContent>
+            <DialogActions>
+              <Button onClick={handleCancelDelete} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleConfirmDelete} color="secondary">
+                Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
 
-          ))
-        )}
-      </Grid>
+          <Dialog
+            open={updateDialogOpen}
+            onClose={handleUpdateDialogClose}
+            aria-labelledby="update-dialog-title"
+            aria-describedby="update-dialog-description"
+          >
+            <DialogTitle id="update-dialog-title">Edit Task</DialogTitle>
+            <DialogContent>
+              <TextField
+                sx={{ mt: 1 }}
+                label="Task"
+                variant="outlined"
+                fullWidth
+                value={taskToUpdate.task}
+                onChange={(e) => setTaskToUpdate({ ...taskToUpdate, task: e.target.value })}
+              />
+              <TextField
+                sx={{ mt: 1 }}
+                label="Description"
+                variant="outlined"
+                fullWidth
+                value={taskToUpdate.description}
+                onChange={(e) => setTaskToUpdate({ ...taskToUpdate, description: e.target.value })}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleUpdateDialogClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleUpdateTask} color="primary">
+                Update
+              </Button>
+            </DialogActions>
+          </Dialog>
 
-      {/* Snackbar for delete success */}
-      <Snackbar open={deleteSuccess} autoHideDuration={3000} onClose={handleDeleteAlertClose}>
-        <Alert onClose={handleDeleteAlertClose} severity="success">
-          Task deleted successfully!
-        </Alert>
-      </Snackbar>
-
-      {/* Delete Confirmation Dialog */}
-      <Dialog
-        open={deleteDialogOpen}
-        onClose={handleCancelDelete}
-        aria-labelledby="delete-dialog-title"
-        aria-describedby="delete-dialog-description"
-      >
-        <DialogTitle id="delete-dialog-title">Confirm Delete</DialogTitle>
-        <DialogContent>Are you sure you want to delete this task?</DialogContent>
-        <DialogActions>
-          <Button onClick={handleCancelDelete} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleConfirmDelete} color="secondary">
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-      {/* Update Task Dialog */}
-      <Dialog
-        open={updateDialogOpen}
-        onClose={handleUpdateDialogClose}
-        aria-labelledby="update-dialog-title"
-        aria-describedby="update-dialog-description"
-      >
-        <DialogTitle id="update-dialog-title">Edit Task</DialogTitle>
-        <DialogContent>
-          <TextField
-            sx={{ mt: 1 }}
-            label="Task"
-            variant="outlined"
-            fullWidth
-            value={taskToUpdate.task}
-            onChange={(e) => setTaskToUpdate({ ...taskToUpdate, task: e.target.value })}
-          />
-          <TextField
-            sx={{ mt: 1 }}
-            label="Description"
-            variant="outlined"
-            fullWidth
-            value={taskToUpdate.description}
-            onChange={(e) => setTaskToUpdate({ ...taskToUpdate, description: e.target.value })}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleUpdateDialogClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleUpdateTask} color="primary">
-            Update
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-    
-      <Dialog
-        open={addDialogOpen}
-        onClose={handleCloseAddDialog}
-        aria-labelledby="add-dialog-title"
-        aria-describedby="add-dialog-description"
-      >
-        <DialogTitle id="add-dialog-title">Add Todo</DialogTitle>
-        <DialogContent>
-          <TextField
-            label="Task"
-            variant="outlined"
-            fullWidth
-            value={newTodo.task}
-            onChange={(e) => setNewTodo({ ...newTodo, task: e.target.value })}
-            sx={{ mt: 1 }}
-          />
-          <TextField
-            label="Description"
-            variant="outlined"
-            fullWidth
-            value={newTodo.description}
-            onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })}
-            sx={{ mt: 1 }}
-          />
-          <DialogContentText>Task Cannot be null.</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseAddDialog} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleAddTodo} color="primary">
-            Add
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Snackbar
-  open={snackbarOpen}
-  autoHideDuration={3000} 
-  onClose={() => setSnackbarOpen(false)}
->
-  <Alert onClose={() => setSnackbarOpen(false)} severity="error">
-    {snackbarMessage}
-  </Alert>
-</Snackbar>
-
-      </div>
+          <Dialog
+            open={addDialogOpen}
+            onClose={handleCloseAddDialog}
+            aria-labelledby="add-dialog-title"
+            aria-describedby="add-dialog-description"
+          >
+            <DialogTitle id="add-dialog-title">Add Todo</DialogTitle>
+            <DialogContent>
+              <TextField
+                label="Task"
+                variant="outlined"
+                fullWidth
+                value={newTodo.task}
+                onChange={(e) => setNewTodo({ ...newTodo, task: e.target.value })}
+                sx={{ mt: 1 }}
+              />
+              <TextField
+                label="Description"
+                variant="outlined"
+                fullWidth
+                value={newTodo.description}
+                onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })}
+                sx={{ mt: 1 }}
+              />
+              <DialogContentText>Task Cannot be null.</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseAddDialog} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleAddTodo} color="primary">
+                Add
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={3000} 
+            onClose={() => setSnackbarOpen(false)}
+          >
+            <Alert onClose={() => setSnackbarOpen(false)} severity="error">
+              {snackbarMessage}
+            </Alert>
+          </Snackbar>
+        </div>
       )}
-
     </div>
   );
 };
